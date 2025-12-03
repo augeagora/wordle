5ba742f5685c6fd6937bb907764b1016811551bc
Douglas Serrano <esdouglasserrano@gmail.com>
2025-12-02 21:02:47 -0800

    A bunch of stuff

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Wordle
+﻿namespace Wordle
 {
   class Program
   {
@@ -11,7 +9,27 @@ namespace Wordle
       static void Start()
       {
         Console.WriteLine("Douglas's Wordle");
+        Console.WriteLine("1 - Play");
+        Console.WriteLine("0 - Exit");
+
+        String choice = Console.ReadLine();
+
+        if (choice.Equals("1"))
+        {
+          Console.Clear();
+          Console.WriteLine("I dunnno...");
+        }
+        else if (choice.Equals("0"))
+        {
+          Console.Clear();
+          Console.WriteLine("Goodbye!");
+          Exit();
+        }
       }
+    }
+    static void Exit()
+    {
+      Environment.Exit(1);
     }
   }
 }
